c20cf3d04207b9b76111e800e9fcaef9742658f9
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2020-02-18 11:53:14 +0900

    [upd] メニューのホバーアニメーションをjsからcssにした

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,1 @@
-window.addEventListener('load', function () {
-	let MenuItems = document.getElementsByClassName('MenuItem');
-	for (let i = 0; i < MenuItems.length; i++) {
-		MenuItems[i].onmouseover = function () {
-			this.style.background = 'lightblue';
-		}
-		MenuItems[i].onmouseleave = function () {
-			this.style.background = 'none';
-		}
-	}
-}
-);
+window.addEventListener('load', function () { });
